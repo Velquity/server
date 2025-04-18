ba4bfdb96928dd056fffaf267a52d14f2824c258
nemesis-9 <nyxn96@gmail.com>
2025-04-18 20:22:37 +0530

    mapping models associations

--- model/index.js
+++ model/index.js
@@ -31,33 +31,41 @@ import Purchase_Product from "./mappings/Purchase_Product.js"
 
 // Cheque Associations
 Cheque.belongsTo(Company, { foreignKey: 'companyId', as: 'company' })
+Cheque.hasOne(Payment_Cheque, { foreignKey: 'chequeId', as: 'payment_cheque' })
 
 // Client Associations
 
 // Company Associations
 Company.hasMany(Cheque, { foreignKey: 'companyId', as: 'cheques' })
 Company.hasMany(Order, { foreignKey: 'companyId', as: 'orders' })
-Company.hasMany(Product, { foreignKey: 'manufacturerId', as: 'products' })
-Company.hasMany(Product, { foreignKey: 'distributorId', as: 'products' })
+Company.hasMany(Product, { foreignKey: 'manufacturerId', as: 'manufacturedProducts' })
+Company.hasMany(Product, { foreignKey: 'distributorId', as: 'distributedProducts' })
 Company.hasMany(Purchase, { foreignKey: 'companyId', as: 'purchases' })
 
 // Order Associations
 Order.belongsTo(Company, { foreignKey: 'companyId', as: 'company' })
+Order.hasMany(Order_Product, { foreignKey: 'orderId', as: 'order_products' })
+Order.hasMany(Order_Purchase, { foreignKey: 'orderId', as: 'order_purchases' })
 
 // Payment Associations
+Payment.hasMany(Payment_Cheque, { foreignKey: 'paymentId', as: 'payment_cheques' })
+Payment.hasMany(Payment_Purchase, { foreignKey: 'paymentId', as: 'payment_purchases' })
 
 // Product Associations
 Product.belongsTo(Company, { foreignKey: 'manufacturerId', as: 'manufacturer' })
 Product.belongsTo(Company, { foreignKey: 'distributorId', as: 'distributor' })
-
+Product.hasMany(Order_Product, { foreignKey: 'productId', as: 'order_products' })
+Product.hasMany(Product_Price, { foreignKey: 'productId', as: 'product_prices' })
+Product.hasMany(Purchase_Product, { foreignKey: 'productId', as: 'purchase_products' })
 
 // Purchase Associations
 Purchase.belongsTo(Company, { foreignKey: 'companyId', as: 'company' })
-
+Purchase.hasMany(Order_Purchase, { foreignKey: 'purchaseId', as: 'order_purchases' })
+Purchase.hasMany(Payment_Purchase, { foreignKey: 'purchaseId', as: 'payment_purchases' })
+Purchase.hasMany(Purchase_Product, { foreignKey: 'purchaseId', as: 'purchase_products' })
 
 // RefreshToken Associations
 RefreshToken.belongsTo(User, { foreignKey: 'userId', as: 'user' })
-
 
 // User Associations
 User.hasMany(RefreshToken, { foreignKey: 'userId', as: 'refreshTokens', onDelete: 'CASCADE' })
@@ -90,22 +98,29 @@ User.hasMany(RefreshToken, { foreignKey: 'userId', as: 'refreshTokens', onDelete
 // *._.*._.*._.*._.* Mapping Associations *._.*._.*._.*._.* \\
 
 // Order_Product Associations
-
+Order_Product.belongsTo(Order, { foreignKey: 'orderId', as: 'order' })
+Order_Product.belongsTo(Product, { foreignKey: 'productId', as: 'product' })
 
 // Order_Purchase Associations
-
+Order_Purchase.belongsTo(Order, { foreignKey: 'orderId', as: 'order' })
+Order_Purchase.belongsTo(Purchase, { foreignKey: 'purchaseId', as: 'purchase' })
 
 // Payment_Cheque Associations
-
+Payment_Cheque.belongsTo(Payment, { foreignKey: 'paymentId', as: 'payments' })
+Payment_Cheque.belongsTo(Cheque, { foreignKey: 'chequeId', as: 'cheques' })
 
 // Payment_Purchase Associations
-
+Payment_Purchase.belongsTo(Payment, { foreignKey: 'paymentId', as: 'payments' })
+Payment_Purchase.belongsTo(Purchase, { foreignKey: 'purchaseId', as: 'purchase' })
 
 // Product_Price Associations
-
+Product_Price.belongsTo(Product, { foreignKey: 'productId', as: 'product' })
+Product_Price.hasMany(Purchase_Product, { foreignKey: 'priceVersion', as: 'purchase' })
 
 // Purchase_Product Associations
-
+Purchase_Product.belongsTo(Purchase, { foreignKey: 'purchaseId', as: 'purchase' })
+Purchase_Product.belongsTo(Product, { foreignKey: 'productId', as: 'product' })
+Purchase_Product.belongsTo(Product_Price, { foreignKey: 'priceVersion', as: 'prices' })
 
 
 // Export models
